2cb40a0358e693afcdafb8409a6fd9c06210def7
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-29 15:45:22 -0500

    add auth services

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
 import { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return <div className='App'>FRY Diagnosis App</div>;
-}
+// Services
+import { getUser, logout } from "../services/authService";
+
+// Pages + Components
+
+const App = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(getUser());
+
+  const handleSignupOrLogin = async () => {
+    const currentUser = await getUser();
+    setUser(currentUser);
+  };
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+    navigate('/');
+  };
+
+  return (
+    <div className='App'>
+      <Nav user={user} handleLogout={handleLogout} />
+      <Routes>
+        <Route path='/' element={<Landing user={user} />} />
+        <Route
+          path='/signin'
+          element={<SignIn handleSignupOrLogin={handleSignupOrLogin} />}
+        />
+        <Route
+          path='/signup'
+          element={<SignUp handleSignupOrLogin={handleSignupOrLogin} />}
+        />
+        <Route path='/study' element={<Study user={user} />} />
+        <Route path='/spellingbee' element={<Spellingbee user={user} />} />
+        <Route path='/profile' element={<Profile user={user} />} />
+        <Route path='/admin' element={<Admin user={user} />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/services/authService.js
@@ -0,0 +1,68 @@
+import * as tokenService from './tokenService';
+const BASE_URL = 'localhost:3000';
+
+async function signup(user) {
+  try {
+    const res = await fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(user),
+    });
+    const json = await res.json();
+    if (json.token) {
+      tokenService.setToken(json.token);
+    }
+    if (json.err) {
+      throw new Error(json.err);
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+async function addStudent(user) {
+  try {
+    const res = await fetch(`${BASE_URL}/addStudent`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+    const json = await res.json();
+    if (json.err) {
+      throw new Error(json.err);
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+function getUser() {
+  return tokenService.getUserFromToken();
+}
+
+function logout() {
+  tokenService.removeToken();
+}
+
+async function login(credentials) {
+  try {
+    const res = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(credentials),
+    });
+    const json = await res.json();
+    if (json.token) {
+      tokenService.setToken(json.token);
+    }
+    if (json.err) {
+      throw new Error(json.err);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+export { signup, getUser, logout, login, addStudent };
