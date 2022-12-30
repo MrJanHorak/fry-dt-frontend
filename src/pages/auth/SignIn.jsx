c2e833d6af414f388aacb0234e46b39b2e2d34c0
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-29 22:49:20 -0500

    add sign in page

---
+++ src/pages/auth/SignIn.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import '../../styles/Auth.css';
+
+// Services
+import { login } from '../../services/authService';
+import ReadQr from '../../components/QrCodeReader/QrCodeReader';
+
+const SignIn = ({ handleSignupOrLogin }) => {
+  const navigate = useNavigate();
+  const [msg, setMsg] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    pw: '',
+  });
+
+  const handleChange = (e) => {
+    setMsg('');
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(formData);
+      handleSignupOrLogin();
+      navigate('/');
+    } catch (error) {
+      setMsg(error.message);
+    }
+  };
+
+  return (
+    <div className='signup-page'>
+      <div className='form-container'>
+        <div className='signin-title-container'>
+          <h1>Sign In</h1>
+          {msg ? <h3>{msg}</h3> : <h3>FRY Diagnosis Tool</h3>}
+        </div>
+        <div className='sign-in-container'>
+          <div className='sign-in-form'>
+            <form className='register-form' onSubmit={handleSubmit}>
+              <input
+                required
+                name='name'
+                type='String'
+                autoComplete='off'
+                placeholder='user name'
+                onChange={handleChange}
+                value={formData.name}
+              />
+              <input
+                required
+                name='pw'
+                type='password'
+                autoComplete='off'
+                placeholder='Password'
+                onChange={handleChange}
+                value={formData.pw}
+              />
+
+              <button autoComplete='off' id='submit-button' type='submit'>
+                SIGN IN
+              </button>
+            </form>
+          </div>
+          <div className='QrCode-signin'>
+            <br />
+            <ReadQr handleSignupOrLogin={handleSignupOrLogin} />
+          </div>
+        </div>
+        <div className='redirect-container'>
+          <p>Don't have an account?</p>
+          <Link className='redirect-link' to='/signup'>
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
